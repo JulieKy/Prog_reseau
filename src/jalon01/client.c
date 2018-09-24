b07dd74135dbf38f0c511bf04687867516e4d205
JulieKY <jujuk33@gmail.com>
2018-09-24 18:56:59 +0200

    write coté client

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -30,12 +30,16 @@ int main(int argc,char** argv)
     //s = do_socket()
     int sock;
     sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock==-1){
+      perror("socket");
+      exit(EXIT_FAILURE);
+    }
 
     // Init the sock_host structure
     struct sockaddr_in sock_host;
     memset(& sock_host, '\0', sizeof(sock_host)); // Initilalisation de l'adresse de la socket à 0
     sock_host.sin_family=AF_INET; // Ipv4
-    sock_host.sin_port=htons(0); // ATTENTION, PEUT ETRE PAS 0 !!!  Le port client est alloué dynamiquement
+    sock_host.sin_port=htons(n_port); // ATTENTION, PEUT ETRE PAS 0 !!!  Le port client est alloué dynamiquement
     int sv_addr_ok;
     sv_addr_ok = inet_aton(sv_addr, &sock_host.sin_addr); // Converti l'adresse en une donnée binaire (renvoie 0 si pas bon)
 
@@ -43,7 +47,7 @@ int main(int argc,char** argv)
 
 
     //connect to remote socket
-    connect(sock,(struct sockaddr*)& sock_host, sizeof(sock_host));
+  connect(sock,(struct sockaddr*)& sock_host, sizeof(sock_host));
 
 
 //get address info from the server
@@ -58,16 +62,18 @@ int main(int argc,char** argv)
 
 //get user input
 //readline()
-char msg;
+char msg[30];
 printf("Que voulez-vous envoyer au serveur?\n");
-scanf("%s",&msg);
-printf("%s",&msg); // PROBLEME : NE PREND QU'UN SEUL MOT
+fgets(msg,30,stdin); // 30 pour avoir une ligne
 
 //send message to the server
 //handle_client_message()
+int sent=0, to_send=strlen(msg);
+do{
+  sent+= write(sock,msg+sent,strlen(msg)-sent);
+} while (sent!=to_send);
 
-
-    return 0;
+return 0;
 
 
 }

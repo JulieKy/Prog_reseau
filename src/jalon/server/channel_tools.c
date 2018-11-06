36c3cbfa767ae8d25a70d4036daf82e8680d07a8
JulieKY <jujuk33@gmail.com>
2018-11-06 16:37:14 +0100

    Jalon4 : Fonctions utiles dans channel_tools mais pas verif

--- src/jalon/server/channel_tools.c
+++ src/jalon/server/channel_tools.c
@@ -15,25 +15,86 @@
 struct channel* channel_list_init() {
   struct channel* list_channel= malloc(sizeof(*list_channel));
   list_channel=NULL;
-  struct clt* list_client = client_list_init();
-  list_channel->client= list_client;
   return list_channel;
 }
 
 
 /* -------------- Create a new channel-------------- */
-struct channel* channel_new(){
+struct channel* channel_new(char* name){
   struct channel* new_channel=malloc(sizeof(*new_channel));
-  new_channel->name="unknown";
-  printf("channel new");
+  new_channel->name=name;
+  struct clt* list_client = client_list_init();
+  new_channel->client= list_client;
   return new_channel;
 }
 
 /* -------------- Add a channel to the list -------------- */
-struct channel* channel_add(struct channel* first_channel){
-  struct channel* new_channel=channel_new();
-  // Ajout en début de liste
+struct channel* channel_add(struct channel* first_channel, char* name){
+
+  printf(">> Addition of a new channel\n");
+
+  struct channel* new_channel=channel_new(name);
   new_channel->next=first_channel;
   first_channel=new_channel;
+
   return first_channel;
+}
+
+/* -------------- Find a channel thanks to his name -------------- */
+struct channel* channel_find_name(struct channel* first_channel, char* name){
+
+  struct channel* found_channel=first_channel;
+
+  if (strcmp(name, found_channel->name)==0)
+    return found_channel;
+
+  while (found_channel!=NULL && (strcmp(name, found_channel->name)!=0)){
+    found_channel=found_channel->next;
+  }
+  return found_channel;
+}
+
+
+/* -------------- Remove a channel from the list -------------- */
+struct channel* remove_channel(struct channel* first_channel, struct channel* removed_channel){
+
+  struct channel* temp = first_channel;
+  if (first_channel==NULL){
+    return NULL;
+  }
+  //Suppression premier element
+  if(temp==removed_channel){
+    return temp->next;
+  }
+
+  while(temp->next!=NULL){
+    //Suppression dernier element
+    if ((temp->next)->next==NULL && temp->next==removed_channel) {
+      temp->next=NULL;
+      break;
+    }
+    // Suppression element au milieu
+    if(temp->next==removed_channel) {
+      temp->next=(temp->next)->next;
+    }
+    temp=temp->next;
+  }
+  free(removed_channel);
+  return first_channel;
+}
+
+
+/* -------------- Return the number of connected clients -------------- */
+int nbre_channel(struct channel* first_channel){
+  if (first_channel==NULL)
+    return 0;
+  else {
+    struct channel* temp=first_channel;
+    int c=1;
+    while (temp->next!=NULL){
+      temp=temp->next;
+      c++;
+    }
+    return c;
+  }
 }
--- src/jalon/server/channel_tools.h
+++ src/jalon/server/channel_tools.h
@@ -10,8 +10,21 @@ struct channel {
 /* -------------- Create a new list of channels-------------- */
 struct channel* channel_list_init();
 
+
 /* -------------- Create a new channel-------------- */
-struct channel* channel_new();
+struct channel* channel_new(char* name);
+
 
 /* -------------- Add a channel to the list -------------- */
-struct channel* channel_add(struct channel*);
+struct channel* channel_add(struct channel*, char* name);
+
+
+/* -------------- Find a channel thanks to his name -------------- */
+struct channel* channel_find_name(struct channel* first_channel, char* name);
+
+
+/* -------------- Remove a channel from the list -------------- */
+struct channel* remove_channel(struct channel* first_channel, struct channel* removed_channel);
+
+/* -------------- Return the number of connected clients -------------- */
+int nbre_channel(struct channel* first_channel);
